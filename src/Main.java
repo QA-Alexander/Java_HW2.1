942a216e64b93bf2d6f79121328f14e5fc0606e9
Alexander Ryzhakov <aryzhakov87@gmail.com>
2024-11-05 22:29:42 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double price = 12786.13;
+        int rubles = 20;
+        int bonus = (int)price / rubles;
+
+        System.out.println("Ваши бонусные мили:" + bonus);
+    }
+}
